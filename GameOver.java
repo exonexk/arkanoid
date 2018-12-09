7ad7b65c8d01287cf2e38a0ebc2e4ba176f84d00
exonexk <44666334+exonexk@users.noreply.github.com>
2018-12-09 02:04:41 +0100

    Game Over screen
    Přidání screen pro konec hry a ukládání skóre do shared preferences.

---
+++ GameOver.java
@@ -0,0 +1,57 @@
+package com.example.lukas.arkanoid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class GameOver extends Activity {
+    Button Mainmenu;
+    Button Save_score;
+    EditText Name;
+    String score;
+    TextView scor;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over);
+
+        Mainmenu = findViewById(R.id.button3);
+        Save_score = findViewById(R.id.save_score);
+        Name = findViewById(R.id.editText);
+        scor = findViewById(R.id.score_view);
+
+        Intent intent;
+        intent = getIntent();
+
+        score = intent.getStringExtra("highscore");
+
+        scor.setText("Your score: " + score);
+
+
+
+
+    }
+
+    public void MainMenu(View view) {
+        Intent intent = new Intent(this, MainMenu.class);
+        startActivity(intent);
+    }
+
+    public void SaveScore(View view) {
+        Intent intent = new Intent(this, Score.class);
+        String nam;
+        nam = Name.getText().toString();
+
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putString("username", nam);
+        editor.putString("score", score);
+        editor.apply();
+        startActivity(intent);
+    }
+}
